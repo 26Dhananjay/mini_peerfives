73a3bcfa7a9ec51ef746502fe056cc199e63a14e
26Dhananjay <djaythorat1991@gmail.com>
2024-11-19 13:04:54 +0530

    Add new users and users list functionality and ui is done

--- app/controllers/reward_histories_controller.rb
+++ app/controllers/reward_histories_controller.rb
@@ -1,2 +1,43 @@
 class RewardHistoriesController < ApplicationController
+  def index_p5
+    @user = User.find(params[:id])
+    @p5_histories = @user.given_rewards
+  end
+
+  def index_rewards
+    @user = User.find(params[:id])
+    @reward_histories = @user.received_rewards
+  end
+
+  def new
+    @user = User.find(params[:id])
+    @other_users = User.where.not(id: @user.id)
+  end
+
+  def create
+    giver = User.find(params[:giver_id])
+    receiver = User.find(params[:receiver_id])
+    points = params[:points].to_i
+
+    if giver.p5_balance >= points
+      RewardHistory.create!(datetime: Time.now, points: points, given_by: giver.id, given_to: receiver.id)
+      giver.update!(p5_balance: giver.p5_balance - points)
+      receiver.update!(reward_balance: receiver.reward_balance + points)
+      redirect_to user_path(giver), notice: 'Reward given successfully!'
+    else
+      redirect_back fallback_location: root_path, alert: 'Insufficient balance!'
+    end
+  end
+
+  def destroy
+    reward = RewardHistory.find(params[:id])
+    giver = reward.giver
+    receiver = reward.receiver
+
+    giver.update!(p5_balance: giver.p5_balance + reward.points)
+    receiver.update!(reward_balance: receiver.reward_balance - reward.points)
+    reward.destroy
+
+    redirect_back fallback_location: root_path, notice: 'Reward reversed!'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  get '/new', to: 'users#new' 
+  resources :users do
+    member do
+      get 'p5', to: 'reward_histories#index_p5'
+      get 'rewards', to: 'reward_histories#index_rewards'
+      get 'rewards/new', to: 'reward_histories#new'
+    end
+  end
+
+  resources :reward_histories, only: [:create, :destroy]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -12,3 +23,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+
